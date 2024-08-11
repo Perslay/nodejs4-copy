c999c14dae782f723501c701ae7e4d7d94ebeecc
Natalia Fedyk <124808493+Perslay@users.noreply.github.com>
2024-08-11 19:27:10 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("./passport");
+require("dotenv").config();
+
+const apiRouter = require("./routes/api/index");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use("/api", apiRouter);
+
+app.use((_, res, __) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, _, res, __) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const MAIN_PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_URL;
+
+const connection = mongoose.connect(uriDb);
+
+connection
+  .then(() => {
+    app.listen(MAIN_PORT, function () {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((err) => {
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit(1);
+  });
